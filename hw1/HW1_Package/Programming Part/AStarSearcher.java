75159d34f757d1e5082ea29a1176b4cfe69fce28
Tyler A Stank <tbear2500@gmail.com>
2015-02-09 10:28:44 -0600

    Implemented A*, I think.

--- hw1/HW1_Package/Programming Part/AStarSearcher.java
+++ hw1/HW1_Package/Programming Part/AStarSearcher.java
@@ -33,14 +33,16 @@ public class AStarSearcher extends Searcher {
 
 		// OPEN list (aka Frontier list)
 		PriorityQueue<StateFValuePair> open = new PriorityQueue<StateFValuePair>();
+        // The h() value need not be tracked, as it can be computed
+        ArrayList<State> expanded = new ArrayList<State>();
 
 		// TODO initialize the root state and add
 		// to OPEN list
 		// ...
         open.add(new StateFValuePair(
                 new State(maze.getPlayerSquare(), null, 0, 0),
-                abs(maze.getPlayerSquare().X - maze.getGoalSquare().X)
-                    + abs(maze.getPlayerSquare().Y - maze.getGoalSquare.y)
+                Math.abs(maze.getPlayerSquare().X - maze.getGoalSquare().X)
+                    + Math.abs(maze.getPlayerSquare().Y - maze.getGoalSquare().y)
         ));
 
 		while (!open.isEmpty()) {
@@ -61,7 +63,28 @@ public class AStarSearcher extends Searcher {
                 return true;
             }
 
-            for (State successor.get) {
+            for (State successor : state.getSuccessors(closed, maze)) {
+                //shit
+                // I'm not keeping track of closed as a boolean here,
+                // because under some circumstances, we'll want to re-add
+                // nodes to the graph.
+                for (State prior : expanded) {
+                    // I could use more conditions here, but it's easer to
+                    // just layer the ifs
+                    if (prior.getX() == successor.getX() && prior.getY() == successor.getY()) {
+                        // we don't need to compute f(), as h() is fixed
+                        // for the x and y coords, which are equal
+                        if (successor.getGValue() < prior.getGValue()) {
+                            // we want to add successor, even though we've
+                            // already visited a node at the same place
+                            open.add(new StateFValuePair(successor,
+                                    successor.getGValue()
+                                    + Math.abs(successor.getX() - maze.getGoalSquare().X)
+                                    + Math.abs(successor.getY() - maze.getGoalSquare().Y)
+                            ));
+                        }
+                    }
+                }
             }
 		}
 
